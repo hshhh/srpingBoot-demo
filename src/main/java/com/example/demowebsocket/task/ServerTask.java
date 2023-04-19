9c32c92d65a729d22e677309a0d6ec3e40558da4
26877 <2687776836@qq.com>
2023-04-19 08:58:51 +0800

    消息推送task

--- src/main/java/com/example/demowebsocket/task/ServerTask.java
+++ src/main/java/com/example/demowebsocket/task/ServerTask.java
@@ -10,25 +10,27 @@ import com.example.demowebsocket.payload.ServerVO;
 import com.example.demowebsocket.util.ServerUtil;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@Component
 @Slf4j
+@Component
 public class ServerTask {
-
+    @Autowired
     private SimpMessagingTemplate wsTemplate;
 
-    @Scheduled(cron="0/2 * * * * ?")
+    @Scheduled(cron = "0/2 * * * * ?")
     public void websocket() throws Exception{
         log.info("消息推送开始执行：{}", DateUtil.formatDateTime(new Date()));
         Server server = new Server();
         server.copyTo();
         ServerVO sv = ServerUtil.wrapServerVO(server);
         Dict dict = ServerUtil.wrapServerDict(sv);
+        log.info("推送的消息是"+JSONUtil.toJsonStr(dict));
         wsTemplate.convertAndSend(WebSocketConsts.PUSH_SERVER,JSONUtil.toJsonStr(dict));
         log.info("消息推送结束执行：{}", DateUtil.formatDateTime(new Date()));
     }
