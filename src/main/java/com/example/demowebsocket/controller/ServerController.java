175f35e5da985835bba867338ba3ac29c7417272
26877 <2687776836@qq.com>
2023-04-21 18:16:11 +0800

    设置广播

--- src/main/java/com/example/demowebsocket/controller/ServerController.java
+++ src/main/java/com/example/demowebsocket/controller/ServerController.java
@@ -6,27 +6,45 @@ import com.example.demowebsocket.common.WebSocketConsts;
 import com.example.demowebsocket.model.Server;
 import com.example.demowebsocket.payload.ServerVO;
 import com.example.demowebsocket.util.ServerUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.concurrent.ThreadPoolExecutor;
 
-@RestController
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Slf4j
+@Controller
 @RequestMapping("/server")
 public class ServerController {
     private SimpMessagingTemplate wsTemplate;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
 
     public ServerController(SimpMessagingTemplate wsTemplate) {
         this.wsTemplate = wsTemplate;
     }
 
-    @GetMapping("/sendMessage/{message}")
-    public void sendMessageToAll() {
-        Dict dict = Dict.create().set("message","here");
-        wsTemplate.convertAndSend(WebSocketConsts.PUSH_SERVER2, JSONUtil.toJsonStr(dict));
+
+    @RequestMapping(path="/greetings", method=POST)
+    @ResponseBody
+    public Dict greet(@RequestParam("greeting") String greeting) {
+        log.info("greet is " +greeting);
+        this.wsTemplate.convertAndSend(WebSocketConsts.PUSH_SERVER2, greeting);
+        Dict res = Dict.create().set("send_status","success");
+        return res;
     }
+
 
     @GetMapping
     public Dict serverInfo() throws Exception{
